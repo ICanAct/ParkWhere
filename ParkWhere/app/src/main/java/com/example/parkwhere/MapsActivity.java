d5e8999569a216e9dc9fbfa06165f2e8160f6f16
Krishna <mkvyas999@gmail.com>
2019-10-18 00:05:22 +0800

    Getting user location implemented. Needs to debug.

--- ParkWhere/app/src/main/java/com/example/parkwhere/MapsActivity.java
+++ ParkWhere/app/src/main/java/com/example/parkwhere/MapsActivity.java
@@ -1,30 +1,97 @@
 package com.example.parkwhere;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
-
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
+import android.util.Log;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.api.Places;
+import java.util.Arrays;
+import java.util.List;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String TAG = "";
     private GoogleMap mMap;
+    private Place apporx_place;
+    private double max_likelihood = 0;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+       apporx_place = getLocation();
     }
 
+    private Place getLocation(){
+        Places.initialize(getApplicationContext(), "AIzaSyDyZWootdcmcMONm6MfBirKqIrOjGuRLoE");
+        PlacesClient placesClient = Places.createClient(this);
+        List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME);
+        FindCurrentPlaceRequest request =
+                FindCurrentPlaceRequest.builder(placeFields).build();
+        // Call findCurrentPlace and handle the response (first check that the user has granted permission).
+        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            placesClient.findCurrentPlace(request).addOnSuccessListener((new OnSuccessListener<FindCurrentPlaceResponse>() {
+                @Override
+                public void onSuccess(FindCurrentPlaceResponse response) {
+                    for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
+                        if (placeLikelihood.getLikelihood() > max_likelihood) {
+                            max_likelihood = placeLikelihood.getLikelihood();
+                            apporx_place = placeLikelihood.getPlace();
+                        }
+
+                    }
+                }
+            })).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    if (exception instanceof ApiException) {
+                        ApiException apiException = (ApiException) exception;
+                        Log.e(TAG, "Place not found: " + apiException.getStatusCode());
+                    }
+                }
+            });
+        } else {
+            // A local method to request required permissions;
+            // See https://developer.android.com/training/permissions/requesting
+
+            getLocationPermission();
+
+        }
+        return apporx_place;
+    }
+
+    private void getLocationPermission(){
+
+           // NEED TO WRITE LOGIC TO ASK USER FOR PERMISSIONS FOR THE ACCESS.
+    }
 
     /**
      * Manipulates the map once available.
@@ -40,8 +107,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng user_pos = apporx_place.getLatLng();
+       // LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(user_pos).title("User Position Marker"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(user_pos));
     }
 }
